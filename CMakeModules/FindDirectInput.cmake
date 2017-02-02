0144ab3a6eda88633ad7b39fcb5aae2b8eb85371
Laurens Voerman <l.voerman@rug.nl>
2017-02-02 09:42:31 +0100

    improved cmake find modules fbx (2017.1) GTA , DirectInput

--- CMakeModules/FindDirectInput.cmake
+++ CMakeModules/FindDirectInput.cmake
@@ -13,36 +13,48 @@
 SET( DIRECTINPUT_FOUND FALSE )
 
 IF( WIN32 )
+    set(PFX86 "PROGRAMFILES(X86)") 
     FIND_PATH( DIRECTINPUT_ROOT_DIR Include/D3D10.h
                PATHS
                $ENV{PATH}
                $ENV{PROGRAMFILES}
+               "$ENV{${PFX86}}/Windows Kits/8.1"
+               "$ENV{ProgramFiles}/Windows Kits/8.1"
+               PATH_SUFFIXES "Include" "Include/um"
     )
     
     FIND_PATH( DIRECTINPUT_INCLUDE_DIR dinput.h
                PATHS
-               ${DIRECTINPUT_ROOT_DIR}/Include
+               ${DIRECTINPUT_ROOT_DIR}
+               PATH_SUFFIXES "Include" "Include/um"
     )
-    
+IF(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    SET(DIRECTINPUT_LIBPATHSUFFIX /lib/x64 /Lib/winv6.3/um/x64)
+ELSE()
+    SET(DIRECTINPUT_LIBPATHSUFFIX /lib/x86 /Lib/winv6.3/um/x86)
+ENDIF()    
     FIND_LIBRARY( DIRECTINPUT_LIBRARY dinput7.lib dinput8.lib
                   PATHS
-                  ${DIRECTINPUT_ROOT_DIR}/lib/x86
+                  ${DIRECTINPUT_ROOT_DIR}
+                  PATH_SUFFIXES ${DIRECTINPUT_LIBPATHSUFFIX}
     )
     
     FIND_LIBRARY( DIRECTINPUT_GUID_LIBRARY dxguid.lib
                   PATHS
-                  ${DIRECTINPUT_ROOT_DIR}/lib/x86
+                  ${DIRECTINPUT_ROOT_DIR}
+                  PATH_SUFFIXES ${DIRECTINPUT_LIBPATHSUFFIX}
     )
     
     FIND_LIBRARY( DIRECTINPUT_ERR_LIBRARY dxerr.lib
                   PATHS
-                  ${DIRECTINPUT_ROOT_DIR}/lib/x86
+                  ${DIRECTINPUT_ROOT_DIR}
+                  PATH_SUFFIXES ${DIRECTINPUT_LIBPATHSUFFIX}
     )
     
     SET( DIRECTINPUT_LIBRARIES
          ${DIRECTINPUT_LIBRARY}
          ${DIRECTINPUT_GUID_LIBRARY}
-         ${DIRECTINPUT_ERR_LIBRARY}
+#         ${DIRECTINPUT_ERR_LIBRARY}
     )
     
     IF ( DIRECTINPUT_INCLUDE_DIR AND DIRECTINPUT_LIBRARIES )
